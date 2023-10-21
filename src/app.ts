c601817b990fba05d4edd7d88c2f4b8c610fa13d
Juan Romero <jryn145@gmail.com>
2023-10-21 13:27:03 -0500

    (config): connected with mongodb cluster

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
+import db from "./config/mongo";
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(router);
+db().then(() => console.log('Mongo db connected...'));
 app.listen(PORT, () => console.log(`listening in port => ${PORT}`));
