033121bf701d3ec1697b3891803991a2f496a5b2
Juan Romero <jryn145@gmail.com>
2023-10-22 18:12:39 -0500

    refactor(env): add frontend enviroment variable in cors

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import db from "./config/mongo";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(cors());
+app.use(cors({ origin: [`${process.env.FRONTEND}`] }));
 app.use(express.json());
 app.use(router);
 db().then(() => console.log('Mongo db connected...'));
