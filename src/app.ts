930446d4d2e4e44a061029e72c5f0a2e192db081
Juan Romero <jryn145@gmail.com>
2023-10-22 20:19:10 -0500

    refactor(init): set mongo db connection bbefore the start of the node server

--- src/app.ts
+++ src/app.ts
@@ -10,5 +10,7 @@ const app = express();
 app.use(cors({ origin: [`${process.env.FRONTEND}`] }));
 app.use(express.json());
 app.use(router);
-db().then(() => console.log('Mongo db connected...'), (error) => console.log(error));
-app.listen(PORT, () => console.log(`listening in port => ${PORT}`));
+db().then(() => {
+    console.log('Mongo db connected...');
+    app.listen(PORT, () => console.log(`listening in port => ${PORT}`));
+}, (error) => console.log(error));
